ae335aaa933f0d1250bf29a40d906ce3aa862c67
Enrico Bocchi <enrico.bocchi@cern.ch>
2018-03-14 10:54:35 +0100

    Config files and scripts to integrate with Up2U SSO

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,18 @@ set -o errexit # Bail out on all errors immediately
 echo ""
 echo "Applying customizations for Up2U..."
 
+### 1. Put in place the Up2U logo
+echo "  .Logo ..."
 mv /var/www/html/cernbox/themes/cernbox/core/img/logo-icon.png /var/www/html/cernbox/themes/cernbox/core/img/logo-icon.png.original
 cp up2u_logo.png /var/www/html/cernbox/themes/cernbox/core/img/logo-icon.png
+
+### 2. Set Up2U Single Sign-On paramters
+# NOTE: Please select 'shibboleth' as authentication method ('AUTH_TYPE') for cernbox Docker image v0.7 and later
+echo "  .SSO Configuration..."
+cp sso_config/attribute-map.xml /etc/shibboleth/attribute-map.xml
+sed "s/%%%CERNBOXGATEWAY_HOSTNAME%%%/${CERNBOXGATEWAY_HOSTNAME}/" sso_config/shibboleth2.xml.template > sso_config/shibboleth2.xml
+cp sso_config/shibboleth2.xml /etc/shibboleth/shibboleth2.xml
+
 
 echo "Done"
 echo ""
