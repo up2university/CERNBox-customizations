399ac428ff2da05af7c634505ae5a14db30c045a
Michał Zimniewicz <michzimny@users.noreply.github.com>
2019-02-06 13:33:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,5 @@ echo "  .Logo ..."
 mv /var/www/html/cernbox/themes/cernbox/core/img/logo-icon.png /var/www/html/cernbox/themes/cernbox/core/img/logo-icon.png.original
 cp up2u_logo.png /var/www/html/cernbox/themes/cernbox/core/img/logo-icon.png
 
-### 2. Set Up2U Single Sign-On paramters
-# NOTE: Please select 'shibboleth' as authentication method ('AUTH_TYPE') for cernbox Docker image v0.7 and later
-echo "  .SSO Configuration..."
-cp sso_config/attribute-map.xml /etc/shibboleth/attribute-map.xml
-sed "s/%%%CERNBOXGATEWAY_HOSTNAME%%%/${CERNBOXGATEWAY_HOSTNAME}/" sso_config/shibboleth2.xml.template > sso_config/shibboleth2.xml
-cp sso_config/shibboleth2.xml /etc/shibboleth/shibboleth2.xml
-sed -i "s/LDAPUserBackend/UserBackendSSOtoLDAP/" /var/www/html/cernbox/config/config.php
-###sed -i "s/LDAPUserBackend/UserBackendSSOtoLDAPNumericUID/" /var/www/html/cernbox/config/config.php
-
-
 echo "Done"
 echo ""
